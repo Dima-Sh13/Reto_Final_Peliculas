5d80757571f85ccee097755662b9f27821bbac85
Dima Shalupnya <dshalupnya@gmail.com>
2025-10-12 14:31:12 +0200

    Final commit

--- create.sql
+++ create.sql
@@ -1,8 +1,10 @@
-CREATE TABLE "movies" (
-	"id"	TEXT NOT NULL UNIQUE,
-	"comments"	TEXT,
-	"rating"	INTEGER,
-	PRIMARY KEY("id")
+CREATE TABLE "comments" (
+	"id"	INTEGER NOT NULL,
+	"movie_id"	TEXT,
+	"comment"	TEXT,
+	"user"	TEXT,
+	"date"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 CREATE TABLE "comments" (
 	"id"	INTEGER NOT NULL,
